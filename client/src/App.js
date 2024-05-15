821f6658f97a21770028f285d8d1dec98454899f
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-05-15 10:44:57 -0400

    added header to addbook page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import BookDetails from './pages/BookDetails/BookDetails';
-import AddBook from './pages/AddBook/AddBook';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import BookDetails from "./pages/BookDetails/BookDetails";
+import AddBook from "./pages/AddBook/AddBook";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/books' element={<Home />} />
-        <Route path='/books/:bookId' element={<BookDetails />} />
-        <Route path='/books/add' element={<AddBook />} />
+        {/* <Route path='/books' element={<Home />} />
+        <Route path='/books/:bookId' element={<BookDetails />} /> */}
+        <Route path="/books/add" element={<AddBook />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/pages/AddBook/AddBook.js
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/Header";
+
+function AddBook() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+export default AddBook;
