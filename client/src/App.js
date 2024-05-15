db60afa64dbe39b3159656b88bc03738153e0a7c
kimntan <kim.thd.nguyen@gmail.com>
2024-05-15 09:41:38 -0400

    Add page routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import BookDetails from './pages/BookDetails/BookDetails';
+import AddBook from './pages/AddBook/AddBook';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route to='/books' element={<Home />} />
+        <Route to='/books/:bookId' element={<BookDetails />} />
+        <Route to='/books/add' element={<AddBook />} />
       </Routes>
     </BrowserRouter>
   )
