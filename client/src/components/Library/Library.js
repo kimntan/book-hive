540880dd684fa29efd1cc082cb7fbc6498faa875
kimntan <kim.thd.nguyen@gmail.com>
2024-05-15 11:52:29 -0400

    Add book component.

--- client/src/components/Library/Library.js
+++ client/src/components/Library/Library.js
@@ -1,9 +1,10 @@
+import Book from '../Book/Book';
 import './Library.scss';
 
 export default function Library() {
   return (
     <div className='library'>
-      
+      <Book />
     </div>
   )
 }
